9813738a5d8ab5872d3cec4559c2647cc184ba30
deuce <henreich09@gmail.com>
2025-09-16 06:19:33 +0800

    can fetch properly now

--- scripts/script.js
+++ scripts/script.js
@@ -8,10 +8,54 @@ async function fetchArt() {
 
     const data = await response.json();
     // console.log(data);
-    return data.twitter;
+    return data;
   } catch (error) {
     console.log(error);
     return [];
   }
 }
 
+async function fetchImgFrom(artist){
+  const arts = await fetchArt();
+  let results = [];
+      for (const key in arts.twitter[artist].characters) {
+        results.push(
+          arts.twitter[artist].characters[key].img,
+        );
+      }
+      console.log(`getting info from ${artist}`);
+      return results;
+}
+
+async function fetchUrlFrom(artist){
+  const arts = await fetchArt();
+  let results = [];
+  for(key in arts.twitter[artist].characters){
+    results.push(
+      arts.twitter[artist].characters[key].url,
+    );
+  }
+  return results;
+}
+
+async function fetchCharacterFrom(artist){
+  const arts = await fetchArt();
+  let results = [];
+
+  for(const key in arts.twitter[artist].characters){
+    results.push([
+      Object.keys(arts.twitter[artist].characters)
+    ]);
+  }
+  return results;
+}
+
+
+
+async function getArtFrom(artist){
+  return{
+    image: await fetchImgFrom(artist), 
+    characters: await fetchCharacterFrom(artist),
+    url: await fetchUrlFrom(artist)
+  }
+}
